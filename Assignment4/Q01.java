9ae87d15a167e2e88240792f1d21b3546b64a714
gedder07 <biswajitpatnaik0738@gmail.com>
2024-05-16 17:00:35 +0530

    Create Q01.java

---
+++ Assignment4/Q01.java
@@ -0,0 +1,17 @@
+package ErrorHandling;
+public class Q01 {
+
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str=null;
+		try {
+			int size=str.length();
+			System.out.println("Size of string is :"+size);
+		}catch(NullPointerException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+}
