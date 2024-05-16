91b41c1b405cda67e61e86cccd9e9af28b36b301
gedder07 <biswajitpatnaik0738@gmail.com>
2024-05-16 16:12:38 +0530

    Create q4.java

---
+++ Assignment1/q4.java
@@ -0,0 +1,27 @@
+public class q4{
+private String model;
+private double price;
+public String getModel() {
+return model;
+}
+public void setModel(String model) {
+this.model = model;
+}
+public double getPrice() {
+return price;
+}
+public void setPrice(double price) {
+this.price = price;
+}
+public String toString() {
+return "Laptop [model=" + model + ", price=" + price + "]";
+}
+}
+public class Main {
+public static void main(String[] args) {
+Laptop laptop = new Laptop();
+laptop.setModel("Dell XPS 15");
+laptop.setPrice(1500);
+System.out.println("Laptop: " + laptop);
+}
+}
